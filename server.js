945df8b76fd7835345e77ca9c1e8c2b4cadf168e
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2024-05-15 23:41:35 -0400

    Se añaden instalaciones como la de express,express-fileupload y nodemon

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ app.listen(3000, () => console.log('Your app listening on port 3000'))
 app.get('/', (req, res) => {
   res.send('Oh wow! this is working =)')
 })
+
+
+// Ruta GET /joyas que devuelve la estructura HATEOAS de todas las joyas almacenadas en la base de datos
+
+
+// Ruta GET /joyas/categoria/:categoria que devuelve solo las joyas correspondientes a la categoría obtenida
+
+
+// Ruta GET /joyas que permite el filtrado por campos de las joyas
+
+
+// Ruta GET para el ordenamiento de las joyas según su valor de forma ascendente o descendente usando Query Strings
+
